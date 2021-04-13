c386bd527f50954fa66bbf10a8b04cb92e0d20ba
BeyoungLIN <BeyoungLIN@users.noreply.github.com>
2021-04-14 01:09:40 +0800

    rm cv2.imshow

--- track.py
+++ track.py
@@ -192,7 +192,7 @@ def detect(opt, save_img=False):
 
             # Stream results
             if view_img:
-                cv2.imshow(p, im0)
+                # cv2.imshow(p, im0)
                 if cv2.waitKey(1) == ord('q'):  # q to quit
                     raise StopIteration
 
